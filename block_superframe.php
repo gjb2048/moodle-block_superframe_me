8e6dd40e26d126441b1bdfbfd3b230abaca823ec
Richard Jones <richardnz@outlook.com>
2020-01-25 13:32:03 +1300

    Added a language string

--- block_superframe.php
+++ block_superframe.php
@@ -78,6 +78,7 @@ class block_superframe extends block_base {
         $this->content->footer = '';
         $this->content->text = get_string('welcomeuser', 'block_superframe',
                 $USER);
+        $this->content->text .= '<p>' . get_string('message', 'block_superframe') . '</p>';
 
         return $this->content;
     }
