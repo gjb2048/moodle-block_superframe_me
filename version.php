89881c4f9550cf990ac25cbda2b5c41aaf4c618f
Richard Jones <richardnz@outlook.com>
2020-03-06 09:18:36 +1300

    Reverted some changes to language strings to match course code

--- version.php
+++ version.php
@@ -31,6 +31,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 // State of block after Week 5 add table manager.
-$plugin->version   = 2020012508;
+$plugin->version   = 2020030601;
 $plugin->requires  = 2017111300; // Minimum Moodle 3.4.
 $plugin->component = 'block_superframe';
